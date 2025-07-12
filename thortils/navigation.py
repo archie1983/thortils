4ae2d6405d4d0f614fabe20043e1f45da320c267
Arturs Elksnis <archie1983@hotmail.co.uk>
2025-07-12 19:38:36 +0100

    Better error message for when plan not found

--- thortils/navigation.py
+++ thortils/navigation.py
@@ -443,8 +443,8 @@ def get_shortest_path_to_object(controller, object_id,
     # plt.show()
 
     if tentative_plan is None:
-        raise ValueError("Plan not found from {} to {}"\
-                         .format((start_position, start_rotation), object_id))
+        raise ValueError("Plan not found from {} to {}, {}"\
+                         .format((start_position, start_rotation, target_position), object_id))
     if len(tentative_plan) == 0:
         final_plan = []   # it appears that the robot is at where it should be
 
